a4d536552a9bc3285071ced7ca37dbe2d3bf361d
Ribhu-7 <ribhughosh7@gmail.com>
2024-09-13 14:27:37 +0530

    Initial Commit

---
+++ MoonshotDemo/MoonshotDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotDemoApp.swift
+//  MoonshotDemo
+//
+//  Created by Apple on 13/09/24.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
