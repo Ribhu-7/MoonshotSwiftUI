5d47f50b0f5cb2d76d9445c54701a68ef6c425b9
Ribhu-7 <ribhughosh7@gmail.com>
2024-09-18 18:59:51 +0530

    commit1

---
+++ MoonshotDemo/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MoonshotDemo
+//
+//  Created by Apple on 13/09/24.
+//
+
+import Foundation
+
+struct Astronaut: Codable,Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
